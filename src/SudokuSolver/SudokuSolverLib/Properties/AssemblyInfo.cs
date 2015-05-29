289812358dd35e5c3a3a99e79acd1de3b0f4224f
Alex Ghiondea <alex.ghiondea@gmail.com>
2015-05-28 21:58:10 -0700

    Setup the nuget spec file for the project.

--- src/SudokuSolver/SudokuSolverLib/Properties/AssemblyInfo.cs
+++ src/SudokuSolver/SudokuSolverLib/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SudokuSolverLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("General Sudoku solver library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SudokuSolverLib")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Alex Ghiondea")]
+[assembly: AssemblyProduct("General Sudoku Solver Library")]
+[assembly: AssemblyCopyright("© Alex Ghiondea 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
